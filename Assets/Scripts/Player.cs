8988b35b952b0d5c9705dda9b7866b48dd07b974
Kyle Stanton <kstanton17@outlook.com>
2019-09-12 10:34:45 -0400

    Added basic player character

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float moveSpeed;
+    public float jumpForce;
+    public float maximumSpeed;
+
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player moves right with D and right arrow
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            rb.AddForce(Vector2.right * moveSpeed);
+
+        // Player moves left with A and left arrow
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            rb.AddForce(Vector2.left * moveSpeed);
+        
+        // Player jumps with W and up arrow and space
+        if (isGrounded && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)))
+            rb.AddForce(Vector2.up * jumpForce);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+            isGrounded = false;
+    }
+}
