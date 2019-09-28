a4bef4328b76eb66951606737792a7cddad3452c
Kyle Stanton <kstanton17@outlook.com>
2019-09-28 10:33:29 -0400

    Improved Throwing Update

--- Assets/Scripts/C4.cs
+++ Assets/Scripts/C4.cs
@@ -4,35 +4,62 @@ using UnityEngine;
 
 public class C4 : MonoBehaviour
 {
-    Rigidbody2D rb;
-
-    public float forceForward;
-    public float forceUpward;
+    // Creates a Rigidbody2D
+        Rigidbody2D rb;
+    // Foce on the X-Axis
+        float forceForward;
+    // Force on the Y-Axis
+        float forceUpward;
+    // Scale for throwing on the X-Axis
+        public float forwardMultiply;
+    // Scale for throwing on the Y-Axis
+        public float upwardMultiply;
+    // Max Forward
+        public float xMax;
+    // Max Upward
+        public float yMax;
     // Detects if object is stuck to the ground
         public bool isStuck;
     // List of tags of objects that can explode to C4
         public List<string> explosionTags = new List<string>();
 
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        // Gets the Rigidbody2D attached to the C4
+            rb = GetComponent<Rigidbody2D>();
 
-        if (GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().isLeft == true)
-            forceForward = -Mathf.Abs(forceForward);
+        // Calculates force based on mouse position compared to player position
+            forceForward = Mathf.Abs(Input.mousePosition.x - GameObject.FindGameObjectWithTag("Player").transform.position.x) * forwardMultiply;
+            forceUpward = Mathf.Abs(Input.mousePosition.y - GameObject.FindGameObjectWithTag("Player").transform.position.y) * upwardMultiply;
 
-        rb.AddForce(new Vector2(forceForward, forceUpward));
-        isStuck = false;
-   
+        // Checks force with maximums
+            if (forceForward > xMax)
+                forceForward = xMax;
+
+            if (forceUpward > yMax)
+                forceUpward = yMax;
+
+        // Checks if the player is facing left
+            if (GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().isLeft == true)
+                forceForward = -Mathf.Abs(forceForward);
+
+        // Throws the C4
+            rb.AddForce(new Vector2(forceForward, forceUpward));
+        //C4 is set to default to not stuck to anything
+            isStuck = false;
     }
 
 
     void Update()
     {
-        if (Input.GetMouseButton(1) && isStuck)
-            Destroy(gameObject);
+        // If C4 is stuck to someting and player detonates it, the C4 will explode
+            if (Input.GetMouseButton(1) && isStuck)
+                Destroy(gameObject);
     }
 
+    // Sticks C4 to floors
    private void OnCollisionEnter2D(Collision2D collider)
     {
 
@@ -44,16 +71,20 @@ public class C4 : MonoBehaviour
     }
 
 
+
     private void OnTriggerStay2D(Collider2D other)
     {
-        if ((explosionTags.Contains(other.gameObject.tag) && Input.GetMouseButtonDown(1) && isStuck))
-        {
-            if (other.gameObject.tag == "FuseBox")
-                GameObject.FindGameObjectWithTag("Manager").GetComponent<LevelManager>().RestartScene("Test_Scene");
+        // If the object in its radius can be destroyed and player has detonated the C4 and the C4 is stuck to the ground
+            if ((explosionTags.Contains(other.gameObject.tag) && Input.GetMouseButtonDown(1) && isStuck))
+            {
+            // If fuse box is destroyed, the game ends
+                if (other.gameObject.tag == "FuseBox")
+                    GameObject.FindGameObjectWithTag("Manager").GetComponent<LevelManager>().RestartScene("Test_Scene");
 
-            Destroy(other.gameObject);
+                // Destroys objects in its radius that can be destroyed
+                    Destroy(other.gameObject);
 
-        }
+            }
 
     }
 }
--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -4,48 +4,82 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    Rigidbody2D rb;
-
-    public float lifeTime;
-    float destruct;
-
-    public float forceForward;
-    public float forceUpward;
-
-    public List<string> explosionTags = new List<string>();
+    // Creates Rigidbody2D
+        Rigidbody2D rb;
+    // LifeTime of the grenade
+        public float lifeTime;
+    //Countdown till destruction
+        float destruct;
+    // Force of the greande X-Axis
+        float forceForward;
+    // Force of the grenade Y-Axis
+        float forceUpward;
+    // Max Forward
+        public float xMax;
+    // Max Upward
+        public float yMax;
+    // Scale of the X-Axis throwing
+     public float forwardMultiply;
+    // Scale of the Y-Axis throwing
+        public float upwardMultiply;
+    // List of tags on objects that can be destroyed
+        public List<string> explosionTags = new List<string>();
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        destruct = lifeTime;
 
+        // Sets destruct timer on
+            destruct = lifeTime;
+
+        // calculate force based on how far mouse is from player
+            forceForward = Mathf.Abs(Input.mousePosition.x - GameObject.FindGameObjectWithTag("Player").transform.position.x) * forwardMultiply;
+            forceUpward = Mathf.Abs(Input.mousePosition.y - GameObject.FindGameObjectWithTag("Player").transform.position.y) * upwardMultiply;
+
+        // Checks force against max distance
+            if (forceForward > xMax)
+                forceForward = xMax;
+
+            if (forceUpward > yMax)
+                forceUpward = yMax;
+
+    // Checks if player is left or right
         if (GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().isLeft == true)
             forceForward = -Mathf.Abs(forceForward);
 
+    // throws grenade
         rb.AddForce(new Vector2(forceForward, forceUpward));
+
+        Debug.Log("Forward: " + forceForward);
+        Debug.Log("Upward: " + forceUpward);
     }
 
     // Update is called once per frame
     void Update()
     {
-        destruct -= Time.deltaTime;
+        // timer counts down
+            destruct -= Time.deltaTime;
 
-        if (destruct <= 0)
-            Destroy(gameObject);
+        // checks if timer has ended and detonates if done
+            if (destruct <= 0)
+                Destroy(gameObject);
     }
 
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if ((explosionTags.Contains(other.gameObject.tag) && destruct <= 0.01f))
-        {
-            if (other.gameObject.tag == "FuseBox")
-                GameObject.FindGameObjectWithTag("Manager").GetComponent<LevelManager>().RestartScene("Test_Scene");
+        // Checks of object in its radius can be destroyed and timer has ended
+            if ((explosionTags.Contains(other.gameObject.tag) && destruct <= 0.01f))
+            {
+            // Checks of the fuse box was destroyed and ends the game if it was
+                if (other.gameObject.tag == "FuseBox")
+                    GameObject.FindGameObjectWithTag("Manager").GetComponent<LevelManager>().RestartScene("Test_Scene");
 
-            Destroy(other.gameObject);
+                // Destroys the objects in radius that can be destroyed
+                    Destroy(other.gameObject);
 
-        }
+            }
      
     }
 }
