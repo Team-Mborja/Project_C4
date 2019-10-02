538cfeb6176c7731fcee8fa1751c7d6fad4fb833
Proche23 <55165736+Proche23@users.noreply.github.com>
2019-10-02 13:13:23 -0400

    Mostly Complete Grenade Cooking

--- Assets/Scripts/Equipment/Grenade.cs
+++ Assets/Scripts/Equipment/Grenade.cs
@@ -28,45 +28,54 @@ public class Grenade : MonoBehaviour
         List<GameObject> inRangeItems = new List<GameObject>();
     // Name of Equipment
         public string itemName;
+    // Has been throw
+        bool isThrown;
+    // Spawn Location
+        GameObject spawnPoint;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spawnPoint = GameObject.FindGameObjectWithTag("Spawn");
+        isThrown = false;
 
         // Sets destruct timer on
             destruct = lifeTime;
 
-        // calculate force based on how far mouse is from player
-            forceForward = Mathf.Abs(Input.mousePosition.x - GameObject.FindGameObjectWithTag("Player").transform.position.x) * forwardMultiply;
-            forceUpward = Mathf.Abs(Input.mousePosition.y - GameObject.FindGameObjectWithTag("Player").transform.position.y) * upwardMultiply;
-
-        // Checks force against max distance
-            if (forceForward > xMax)
-                forceForward = xMax;
-
-            if (forceUpward > yMax)
-                forceUpward = yMax;
-
-    // Checks if player is left or right
-        if (GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().isLeft == true)
-            forceForward = -Mathf.Abs(forceForward);
-
-    // throws grenade
-        rb.AddForce(new Vector2(forceForward, forceUpward));
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
         // timer counts down
             destruct -= Time.deltaTime;
+
+        // calculate force based on how far mouse is from player
+        forceForward = Mathf.Abs(Input.mousePosition.x - GameObject.FindGameObjectWithTag("Player").transform.position.x) * forwardMultiply;
+        forceUpward = Mathf.Abs(Input.mousePosition.y - GameObject.FindGameObjectWithTag("Player").transform.position.y) * upwardMultiply;
+
+        // Checks force against max distance
+        if (forceForward > xMax)
+            forceForward = xMax;
+
+        if (forceUpward > yMax)
+            forceUpward = yMax;
+
+        // Checks if player is left or right
+        if (GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().isLeft == true)
+            forceForward = -Mathf.Abs(forceForward);
+
+        if (Input.GetMouseButtonUp(0) && isThrown == false) {
+            rb.AddForce(new Vector2(forceForward, forceUpward));
+            isThrown = true;
+        }
 
         // checks if timer has ended and detonates if done
         if (destruct <= 0)
             Explode();
-
 
     }
 
