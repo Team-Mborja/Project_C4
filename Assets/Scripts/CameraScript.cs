d4de01ab983ae511c1a57a8febb9cf2cbb1f4a1a
Kyle Stanton <kpstanton.2017@mymail.becker.edu>
2019-10-17 13:38:17 -0400

    Level Manager Update

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -6,12 +6,18 @@ public class CameraScript : MonoBehaviour
 {
     // Item replacing the mouse cursor in game
         public GameObject cursor;
+    // Player Object
+        GameObject player;
     // Location of the mouse
         Vector3 target;
+    // Max X Disance 
+        public float xMax;
     void Start()
     {
         // Makes the mouse cursor invisible in the game
             Cursor.visible = false;
+
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Gets the positon of the mouse in world position and puts the new cursor thereS
@@ -21,5 +27,12 @@ public class CameraScript : MonoBehaviour
         target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
         target = new Vector3(-target.x, -target.y, target.z);
         cursor.transform.position = new Vector2(target.x, target.y);
+
+        if(Mathf.Abs(cursor.transform.position.x - player.transform.position.x) > xMax)
+        {
+            //if(player.GetComponent<Player>().isLeft == false)
+               // cursor.transform.position.x = player.transform.position.x + xMax;
+
+        }
     }
 }
