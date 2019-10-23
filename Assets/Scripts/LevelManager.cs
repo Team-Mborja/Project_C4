e2788ea4f8483837c40adc31a5560187dfd7b89a
Kyle Stanton <kpstanton.2017@mymail.becker.edu>
2019-10-22 21:35:17 -0400

    LeftOver Update

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -31,7 +31,7 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timerText.text = timer.ToString();
+        timerText.text = Mathf.Round(timer).ToString();
 
         // runs the level timer when the level is incomplete
             if (fuseBox != null)
