0f17c3f993b13f6603ba37ec271e60b061ed875a
Proche23 <55165736+Proche23@users.noreply.github.com>
2020-01-22 12:20:17 -0500

    created a camera follow script

---
+++ Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform player;
+    public float cameraDistance = 30.00f;
+
+    void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+
+}
