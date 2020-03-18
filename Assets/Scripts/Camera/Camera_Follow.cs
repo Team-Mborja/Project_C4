51a048a910045877da111bbcc2e8a62c3af86778
LucasPellicer <45044981+LucasPellicer@users.noreply.github.com>
2020-03-17 23:21:24 -0400

    Revert "Camera Follows Verticaly"
    This reverts commit 2a3fc29535f96aad3f84ca9b4979b621fca071bc.

--- Assets/Scripts/Camera/Camera_Follow.cs
+++ Assets/Scripts/Camera/Camera_Follow.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class Camera_Follow : MonoBehaviour
 {
-    Vector2 playerPos;
-    
+    private Transform playerTransform;
+
+    public float offset;
+
     public bool bounds;
 
     public Vector3 minCameraPos;
     public Vector3 maxcameraPos;
 
-
-    void Update()
-    {
-        playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
-        transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);  
+    void Start () {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
+
+    //called after update and fixed update
+    void LateUpdate()
+    {
+        if (playerTransform != null)
+        {
+
+            // we store current camera's positon in variable temporary
+            Vector3 temp = transform.position;
+
+            //we set the players camera's position equal to the player's position x
+            temp.x = playerTransform.position.x;
+
+            //temp.y = playerTransform.position.y;
+            //temp.y += offset;
+
+
+            //this will add the offset value to the temp camera pos
+            temp.x += offset;
+
+            //we set back the camera's temp pos to the camera's current pos
+            transform.position = temp;
+
+        }
+    }
     void fixedupdate()
     {
         if (bounds)
